79072ac1bdbbfc927ba30d727f429629816f1697
Ilya Okonechnikov <ok.ilya95kazan@mail.ru>
2021-10-04 13:21:29 +0200

    initial commit

---
+++ src/main/java/com/jaxel/aws/sqsreceiver/SQSReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.jaxel.aws.sqsreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SQSReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SQSReceiverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jaxel/aws/sqsreceiver/SQSReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaxel.aws.sqsreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SQSReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
